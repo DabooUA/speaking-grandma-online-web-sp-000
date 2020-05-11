8fe602b441e54c8555f39cd808eb8a3228676bd9
Serhiy Bardysh <sbardysh@tutanota.com>
2020-05-11 01:14:03 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
-def speak_to_grandma(phrase)
-  
-  if phrase == "I LOVE YOU GRANDMA!"
+def speak_to_grandma(sentence)
+
+  if sentence == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
 
-  elsif phrase == phrase.upcase
+  elsif sentence == sentence.upcase
     return "NO, NOT SINCE 1938!"
 
   else
